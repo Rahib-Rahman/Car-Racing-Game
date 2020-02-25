2c3fd762c787dd12f476d0bf9b2da29860251135
Sheikh Md. Rahibur Rahman <61474527+Rahib-Rahman@users.noreply.github.com>
2020-02-26 05:06:10 +0600

    Add files via upload

---
+++ src/carrace/CarRace.java
@@ -0,0 +1,20 @@
+package carrace;
+
+import com.neub.car.GameFrame;
+
+/**
+ *
+ * @author Sheikh Md. Rahibur Rahman
+ */
+
+public class CarRace {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GameFrame gameFrame = new GameFrame();
+    }
+    
+}
